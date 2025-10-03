075998d8dcc3b4f6185a0962db435c7a63c660ea
Alex Engstrom <aengstrom@utah.gov>
2025-10-03 15:47:32 -0700

    added screening functions to dataanalysis code

--- validationtools/dataanalysis.py
+++ validationtools/dataanalysis.py
@@ -18,7 +18,75 @@ import os
 import pubchempy as pcp
 from collections import defaultdict
 #%% Reference Tables
-mdls = {
+#%%% Compound chemical categories
+compound_categories = {
+    "Ethane": "ALKANE",
+    "Ethylene": "ALKENE",
+    "Propane": "ALKANE",
+    "Propylene": "ALKENE",
+    "Iso-butane": "ALKANE",
+    "N-butane": "ALKANE",
+    "Acetylene": "ALKYNE",
+    "Trans-2-butene": "ALKENE",
+    "1-butene": "ALKENE",
+    "Cis-2-butene": "ALKENE",
+    "Cyclopentane": "ALKANE",
+    "Iso-pentane": "ALKANE",
+    "N-pentane": "ALKANE",
+    "1,3-butadiene": "ALKENE",
+    "Trans-2-pentene": "ALKENE",
+    "1-pentene": "ALKENE",
+    "Cis-2-pentene": "ALKENE",
+    "2,2-dimethylbutane": "ALKANE",
+    "2,3-dimethylbutane": "ALKANE",
+    "2-methylpentane": "ALKANE",
+    "3-methylpentane": "ALKANE",
+    "Isoprene": "TERPENE",
+    "2-methyl-1-pentene": "ALKENE",
+    "1-hexene": "ALKENE",
+    "N-hexane": "ALKANE",
+    "Methylcyclopentane": "ALKANE",
+    "2,4-dimethylpentane": "ALKANE",
+    "Benzene": "AROMATIC",
+    "Cyclohexane": "ALKANE",
+    "2-methylhexane": "ALKANE",
+    "2,3-dimethylpentane": "ALKANE",
+    "3-methylhexane": "ALKANE",
+    "2,2,4-trimethylpentane": "ALKANE",
+    "N-heptane": "ALKANE",
+    "Methylcyclohexane": "ALKANE",
+    "2,3,4-trimethylpentane": "ALKANE",
+    "Toluene": "AROMATIC",
+    "2-methylheptane": "ALKANE",
+    "3-methylheptane": "ALKANE",
+    "N-octane": "ALKANE",
+    "Ethylbenzene": "AROMATIC",
+    "M&p-xylene": "AROMATIC",
+    "Styrene": "AROMATIC",
+    "O-xylene": "AROMATIC",
+    "N-nonane": "ALKANE",
+    "Iso-propylbenzene": "AROMATIC",
+    "Alpha-pinene": "TERPENE",
+    "N-propylbenzene": "AROMATIC",
+    "M-ethyltoluene": "AROMATIC",
+    "P-ethyltoluene": "AROMATIC",
+    "1,3,5-tri-m-benzene": "AROMATIC",
+    "O-ethyltoluene": "AROMATIC",
+    "Beta-pinene": "TERPENE",
+    "1,2,4-tri-m-benzene": "AROMATIC",
+    "N-decane": "ALKANE",
+    "1,2,3-tri-m-benzene": "AROMATIC",
+    "M-diethylbenzene": "AROMATIC",
+    "P-diethylbenzene": "AROMATIC",
+    "N-undecane": "ALKANE",
+    "N-dodecane": "ALKANE"
+}
+def sort_by_type(compound_categories):
+    compound_df = pd.DataFrame(list(compound_categories.items()), columns=['compound', 'category'])
+    group_by_category = compound_df.groupby(['category'])['compound'].apply(lambda x: [aqs_compound_codes[c] for c in x])
+    return group_by_category
+#%%% MDL
+rb_mdls = {
     "Ethane": 0.078055655,
     "Ethylene": 0.127390085,
     "Propane": 0.10797866,
@@ -82,6 +150,69 @@ mdls = {
     "TNMTC": 10,
     "TNMHC": 10
 }
+bv_mdls = {
+    "Ethane": 0.0711,
+    "Ethylene": 0.0642,
+    "Propane": 0.0561,
+    "Propylene": 0.0668,
+    "Iso-butane": 0.0793,
+    "N-butane": 0.0546,
+    "Acetylene": 0.0512,
+    "Trans-2-butene": 0.0409,
+    "1-butene": 0.0604,
+    "Cis-2-butene": 0.0543,
+    "Cyclopentane": 0.0526,
+    "Iso-pentane": 0.0583,
+    "N-pentane": 0.0324,
+    "1,3-butadiene": 0.0366,
+    "Trans-2-pentene": 0.0484,
+    "1-pentene": 0.032,
+    "Cis-2-pentene": 0.0506,
+    "2,2-dimethylbutane": 0.0529,
+    "2,3-dimethylbutane": 0.0448,
+    "2-methylpentane": 0.042,
+    "3-methylpentane": 0.0486,
+    "Isoprene": 0.0293,
+    "2-methyl-1-pentene": 0.0512,
+    "1-hexene": 0.0493,
+    "N-hexane": 0.0593,
+    "Methylcyclopentane": 0.0672,
+    "2,4-dimethylpentane": 0.0848,
+    "Benzene": 0.0525,
+    "Cyclohexane": 0.0747,
+    "2-methylhexane": 0.0896,
+    "2,3-dimethylpentane": 0.0609,
+    "3-methylhexane": 0.0586,
+    "2,2,4-trimethylpentane": 0.0535,
+    "N-heptane": 0.0615,
+    "Methylcyclohexane": 0.0555,
+    "2,3,4-trimethylpentane": 0.0478,
+    "Toluene": 0.0674,
+    "2-methylheptane": 0.0495,
+    "3-methylheptane": 0.0778,
+    "N-octane": 0.1171,
+    "Ethylbenzene": 0.0619,
+    "M&p-xylene": 0.2749,
+    "Styrene": 0.0546,
+    "O-xylene": 0.0586,
+    "N-nonane": 0.0393,
+    "Iso-propylbenzene": 0.0533,
+    "Alpha-pinene": 0.2124,
+    "N-propylbenzene": 0.0652,
+    "M-ethyltoluene": 0.0953,
+    "P-ethyltoluene": 0.0285,
+    "1,3,5-tri-m-benzene": 0.0636,
+    "O-ethyltoluene": 0.2738,
+    "Beta-pinene": 0.1743,
+    "1,2,4-tri-m-benzene": 0.2596,
+    "N-decane": 0.0656,
+    "1,2,3-tri-m-benzene": 0.067,
+    "M-diethylbenzene": 0.077,
+    "P-diethylbenzene": 0.1857,
+    "N-undecane": 0.1558,
+    "N-dodecane": 0.329
+}
+#%%% AQS upload file format
 aqs_file_guide = {"RD": 
                       [
     "Transaction Type",
@@ -144,6 +275,7 @@ aqs_file_guide = {"RD":
     "Uncertainty Value"
                         ]
                 }
+#%%% AQS compound codes
 aqs_compound_codes = {
     "Ethane": 43202,
     "Ethylene": 43203,
@@ -208,70 +340,69 @@ aqs_compound_codes = {
     "TNMTC": 43102,
     "TNMHC": 43000
 }
-plot_compounds = [
-    "Ethane",
-    "Ethylene",
-    "Propane",
-    "Propylene",
-    "Iso-butane",
-    "n-Butane",
-    "Acetylene",
-    "trans-2-Butene",
-    "1-Butene",
-    "cis-2-Butene",
-    "Cyclopentane",
-    "Iso-pentane",
-    "n-Pentane",
-    "1,3-Butadiene",
-    "trans-2-Pentene",
-    "1-Pentene",
-    "cis-2-Pentene",
-    "2,2-Dimethylbutane",
-    "2,3-Dimethylbutane",
-    "2-Methylpentane",
-    "3-Methylpentane",
-    "Isoprene",
-    #"2-Methyl-1-Pentene",    #Not reported to AQS
-    "1-Hexene"
-    ]
-bp_compounds = [
-        "n-Hexane",
-        "Methylcyclopentane",
-        "2,4-Dimethylpentane",
-        "Benzene",
-        "Cyclohexane",
-        "2-Methylhexane",
-        "2,3-Dimethylpentane",
-        "3-Methylhexane",
-        "2,2,4-Trimethylpentane",
-        "n-Heptane",
-        "Methylcyclohexane",
-        "2,3,4-Trimethylpentane",
-        "Toluene",
-        "2-Methylheptane",
-        "3-Methylheptane",
-        "n-Octane",
-        "Ethylbenzene",
-        "m&p-Xylene",
-        "Styrene",
-        "o-Xylene",
-        "n-Nonane",
-        "Iso-propylbenzene",
-        #"alpha-Pinene",       #Not reported to AQS
-        "n-Propylbenzene",
-        "m-ethyltoluene",
-        "p-Ethyltoluene",
-        "1,3,5-Tri-m-benzene",
-        "o-Ethyltoluene",
-        #"beta-Pinene",     #Not reported to AQS
-        "1,2,4-Tri-m-benzene",
-        "n-Decane",
-        "1,2,3-Tri-m-benzene",
-        "m-Diethylbenzene",
-        "p-Diethylbenzene",
-        "n-Undecane",
-        "n-Dodecane"
-    ]
+
+aqs_compound_codes_reversed = {43202: 'Ethane', 
+                               43203: 'Ethylene', 
+                               43204: 'Propane', 
+                               43205: 'Propylene', 
+                               43214: 'Iso-butane', 
+                               43212: 'N-butane', 
+                               43206: 'Acetylene', 
+                               43216: 'Trans-2-butene', 
+                               43280: '1-butene', 
+                               43217: 'Cis-2-butene', 
+                               43242: 'Cyclopentane', 
+                               43221: 'Iso-pentane', 
+                               43220: 'N-pentane', 
+                               43218: '1,3-butadiene', 
+                               43226: 'Trans-2-pentene', 
+                               43224: '1-pentene', 
+                               43227: 'Cis-2-pentene', 
+                               43244: '2,2-dimethylbutane', 
+                               43284: '2,3-dimethylbutane',
+                               43285: '2-methylpentane', 
+                               43230: '3-methylpentane', 
+                               43243: 'Isoprene',
+                               43246: '2-methyl-1-pentene', 
+                               43245: '1-hexene', 
+                               43231: 'N-hexane', 
+                               43262: 'Methylcyclopentane', 
+                               43247: '2,4-dimethylpentane', 
+                               45201: 'Benzene',
+                               43248: 'Cyclohexane', 
+                               43263: '2-methylhexane',
+                               43291: '2,3-dimethylpentane', 
+                               43249: '3-methylhexane', 
+                               43250: '2,2,4-trimethylpentane', 
+                               43232: 'N-heptane', 
+                               43261: 'Methylcyclohexane', 
+                               43252: '2,3,4-trimethylpentane',
+                               45202: 'Toluene', 
+                               43960: '2-methylheptane', 
+                               43253: '3-methylheptane', 
+                               43233: 'N-octane', 
+                               45203: 'Ethylbenzene', 
+                               45109: 'M&p-xylene', 
+                               45220: 'Styrene', 
+                               45204: 'O-xylene', 
+                               43235: 'N-nonane', 
+                               45210: 'Iso-propylbenzene', 
+                               43256: 'Alpha-pinene', 
+                               45209: 'N-propylbenzene', 
+                               45212: 'M-ethyltoluene', 
+                               45213: 'P-ethyltoluene', 
+                               45207: '1,3,5-tri-m-benzene',
+                               45211: 'O-ethyltoluene', 
+                               43257: 'Beta-pinene', 
+                               45208: '1,2,4-tri-m-benzene', 
+                               43238: 'N-decane', 
+                               45225: '1,2,3-tri-m-benzene', 
+                               45218: 'M-diethylbenzene',
+                               45219: 'P-diethylbenzene', 
+                               43954: 'N-undecane', 
+                               43141: 'N-dodecane', 
+                               43102: 'TNMTC', 
+                               43000: 'TNMHC'}
 def reverse_aqs_compound_codes(aqs_code_dict = {
     "Ethane": 43202,
     "Ethylene": 43203,
@@ -337,6 +468,72 @@ def reverse_aqs_compound_codes(aqs_code_dict = {
     "TNMHC": 43000
     }):
     return {value : key for key, value in aqs_code_dict.items()}
+#%%% Compounds by column
+plot_compounds = [
+    "Ethane",
+    "Ethylene",
+    "Propane",
+    "Propylene",
+    "Iso-butane",
+    "n-Butane",
+    "Acetylene",
+    "trans-2-Butene",
+    "1-Butene",
+    "cis-2-Butene",
+    "Cyclopentane",
+    "Iso-pentane",
+    "n-Pentane",
+    "1,3-Butadiene",
+    "trans-2-Pentene",
+    "1-Pentene",
+    "cis-2-Pentene",
+    "2,2-Dimethylbutane",
+    "2,3-Dimethylbutane",
+    "2-Methylpentane",
+    "3-Methylpentane",
+    "Isoprene",
+    #"2-Methyl-1-Pentene",    #Not reported to AQS
+    "1-Hexene"
+    ]
+bp_compounds = [
+        "n-Hexane",
+        "Methylcyclopentane",
+        "2,4-Dimethylpentane",
+        "Benzene",
+        "Cyclohexane",
+        "2-Methylhexane",
+        "2,3-Dimethylpentane",
+        "3-Methylhexane",
+        "2,2,4-Trimethylpentane",
+        "n-Heptane",
+        "Methylcyclohexane",
+        "2,3,4-Trimethylpentane",
+        "Toluene",
+        "2-Methylheptane",
+        "3-Methylheptane",
+        "n-Octane",
+        "Ethylbenzene",
+        "m&p-Xylene",
+        "Styrene",
+        "o-Xylene",
+        "n-Nonane",
+        "Iso-propylbenzene",
+        #"alpha-Pinene",       #Not reported to AQS
+        "n-Propylbenzene",
+        "m-ethyltoluene",
+        "p-Ethyltoluene",
+        "1,3,5-Tri-m-benzene",
+        "o-Ethyltoluene",
+        #"beta-Pinene",     #Not reported to AQS
+        "1,2,4-Tri-m-benzene",
+        "n-Decane",
+        "1,2,3-Tri-m-benzene",
+        "m-Diethylbenzene",
+        "p-Diethylbenzene",
+        "n-Undecane",
+        "n-Dodecane"
+    ]
+
 def plot_compound_list_generator():
     plot_cid_list = [
     6324,  # Ethane
@@ -848,7 +1045,7 @@ class DetectionLimitCombined:
     def __init__(self):
         self.detectionlimits = None
 
-#%% Data Analysis Functions
+#%% AQS txt file parsers
 def generate_df_from_aqs_file(aqs_file, transaction_type: str):
     column_names = aqs_file_guide[transaction_type]
     return pd.read_csv(aqs_file, names = column_names, index_col = False, sep = "|", dtype = 'str')
@@ -939,24 +1136,110 @@ def summarize_qualifiers(aqs_file):
     )
     mask = ~summary['code'].isin(['MD', 'SQ', 'ND', 'LB'])
     return summary[mask]
-    
-# #%% Main
-# if __name__ == "__main__":
-#     #full_hour_nulls, nulls_by_count = summarize_nulls(r"C:\Users\aengstrom\Desktop\RD_SITE-RB_RedButte_INSERT_08-01-2025_to_08-31-2025.txt")
-#     #blank_dict = summarize_blanks(aqs_file=r"C:\Users\aengstrom\Desktop\RB_SITE-RB_RedButte_INSERT_08-01-2025_to_08-31-2025.txt")
-#     aqs_file_rd = r"C:\Users\aengstrom\Desktop\RD_SITE-RB_RedButte_INSERT_08-01-2025_to_08-31-2025.txt"
-#     code_to_name = dt.reverse_aqs_compound_codes()
-#     qualifiers = dt.summarize_qualifiers(aqs_file_rd)
-#     for code in qualifiers['code'].unique():
-#         print(f"- **{code}**")
-#         mask = qualifiers['code'] == code
-#         masked = qualifiers[mask]
-#         for index, row in masked.iterrows():
-#             compound_code = int(row['parameter'])
-#             hours = row['count']
-#             voc_name =  code_to_name.get(compound_code)
-#             print(f"  - {voc_name} : {hours} hour(s) qualified")
 
+#%% Screening checks
+def check_totals(voc_df):
+    required_cols = ['DATE/TIME', '43102', '43000']
+    for col in required_cols:
+        if col not in voc_df.columns:
+            raise ValueError(f"Missing required column: {col}")
+
+    totals_df = voc_df[required_cols]
+
+    # Avoid division by zero
+    nonzero = totals_df['43000'] != 0
+
+    conditions = [
+        (totals_df['43000'] <= 5, 'tnmhc_lt_5'),
+        (totals_df['43000'] >= 500, 'tnmhc_gt_500'),
+        (totals_df['43000'] >= totals_df['43102'], 'tnmhc_gt_tnmtc'),
+        (((totals_df['43000'] - totals_df['43102']) / totals_df['43000'] >= 0.15) & nonzero, 'tnmtc_tnmhc_diff_gt_15'),
+    ]
+
+    flagged = []
+    for cond, label in conditions:
+        subset = totals_df.loc[cond].copy()
+        subset['screen_reason'] = label
+        subset['compounds'] = 'totals'
+        flagged.append(subset)
+
+    totals_check = pd.concat(flagged, ignore_index=True)
+    return totals_check[['DATE/TIME', 'screen_reason','compounds']].copy()
+def check_outliers(voc_df, summary_stats_df):
+    # convert compound ID number column names to compound names
+    voc_df_names = voc_df.copy()
+    voc_df_names.columns = [voc_df_names.columns[0]] + [aqs_compound_codes_reversed[int(c)].upper() for c in voc_df_names.columns[1:]]
+    voc_df_names.iloc[:,1:] = voc_df_names.iloc[:,1:].apply(pd.to_numeric)
+    summary_stats_df.rename(columns = {'Date':'statistic'},inplace=True)
+    stats_clean = summary_stats_df.loc[summary_stats_df['statistic'].isin(["Mean", "Deviation"])].copy()
+    stats_clean.iloc[:,1:] = stats_clean.iloc[:,1:].apply(pd.to_numeric)
+    upper = stats_clean.loc[stats_clean["statistic"] == "Mean"].iloc[0, 1:] + 3 * stats_clean.loc[stats_clean["statistic"] == "Deviation"].iloc[0, 1:]
+    lower = stats_clean.loc[stats_clean["statistic"] == "Mean"].iloc[0, 1:] - 3 * stats_clean.loc[stats_clean["statistic"] == "Deviation"].iloc[0, 1:]
+
+    # Create rows with labels
+    upper_row = pd.DataFrame([["upper", *upper.values]], columns=stats_clean.columns)
+    lower_row = pd.DataFrame([["lower", *lower.values]], columns=stats_clean.columns)
+
+    # Append them
+    stats_clean = pd.concat([stats_clean, upper_row, lower_row], ignore_index=True)
+    upper = stats_clean.set_index("statistic").loc["upper"]
+    lower = stats_clean.set_index("statistic").loc["lower"]
+    
+
+    mask = voc_df_names.drop(columns=['DATE/TIME', 'TNMHC', 'TNMTC']).gt(upper) | voc_df_names.drop(columns=['DATE/TIME', 'TNMHC', 'TNMTC']).lt(lower)
+    flags = pd.concat([voc_df_names['DATE/TIME'], mask], axis=1)
+    mask_any = flags.drop(columns="DATE/TIME").any(axis=1)
+    flags_filtered = flags.loc[mask_any].copy()
+    flags_filtered["compounds"] = flags_filtered.drop(columns="DATE/TIME").apply(
+    lambda row: list(row.index[row]), axis=1
+    )
+    summary = flags_filtered[["DATE/TIME", "compounds"]].copy()
+    summary.insert(loc = 1, column = 'screen_reason', value = 'outliers')
+    return summary
+def check_ratios(voc_df, mdls):
+    mdl_number = {aqs_compound_codes[name]: mdl for name, mdl in mdls.items()}
+    voc_df.iloc[:,1:] = voc_df.iloc[:,1:].apply(pd.to_numeric)
+    compound_by_type = sort_by_type(compound_categories = compound_categories)
+    alkanes = [str(compound) for compound in compound_by_type['ALKANE']]
+    alkenes = [str(compound) for compound in compound_by_type['ALKENE']]
+    conditions = [
+        ((voc_df['45201'] > voc_df['45202']) & (voc_df['45201'] > 3*mdl_number[45201]), 'benzene_gt_toluene'),
+        ((voc_df['45201'] > voc_df['43202']) & (voc_df['45201'] > 3*mdl_number[45201]), 'benzene_gt_ethane'),
+        ((voc_df['43203'] > voc_df['43202']) & (voc_df['43203'] > 3*mdl_number[43203]), 'ethylene_gt_ethane'),
+        ((voc_df['43205'] > voc_df['43204']) & (voc_df['43205'] > 3*mdl_number[43205]), 'propylene_gt_propane'),
+        ((voc_df['45204'] > voc_df['45109']) & (voc_df['45204'] > 3*mdl_number[45204]), 'oxylene_gt_mpxylene'),
+        ((voc_df['43263'] < voc_df['43291']) & (voc_df['43291'] > 3*mdl_number[43291]), '23dimethylpentane_gt_2methylhexane'),
+        ((voc_df['43262'] < voc_df['43247']) & (voc_df['43247'] > 3*mdl_number[43247]), '24dimethylpentane_gt_methylcyclopentane'),
+        ((voc_df['43214'] > voc_df['43212']) & (voc_df['43214'] > 3*mdl_number[43214]), 'isobutane_gt_nbutane'),
+        ((voc_df['43230'] > .6*voc_df['43285']) & (voc_df['43230'] > 3*mdl_number[43230]), '3methylpentane_gt_2methylpentane'),
+        ((voc_df['43954'] > voc_df['43141']) & (voc_df['43954'] > 3*mdl_number[43954]), 'nundecane_gt_ndecane'),
+        (~((voc_df['43221'] > voc_df['43220']) & (voc_df['43220'] > voc_df['43242'])) & (voc_df['43221'] > 3*mdl_number[43221]) & (voc_df['43220'] > 3*mdl_number[43220]) & (voc_df['43242'] > 3*mdl_number[43242]), 'not_isopentane_gt_npentane_gt_cyclopentane'),
+        (voc_df[alkenes].sum(axis = 1) > voc_df[alkanes].sum(axis = 1), 'alkenes_gt_alkanes') 
+         ]
+    flagged = []
+    for cond, label in conditions:
+        subset = voc_df.loc[cond].copy()
+        subset['screen_reason'] = label
+        subset['compounds'] = 'ratios'
+        flagged.append(subset)
+
+    ratios_check = pd.concat(flagged, ignore_index=True)
+    return ratios_check[['DATE/TIME', 'screen_reason','compounds']].copy()  
+
+def monthly_screening_check(ambient_csv, summary_stats_cvs, mdls):
+    summary_stats_df = pd.read_csv(summary_stats_cvs, header = 0, nrows = 5)
+    voc_df = pd.read_csv(ambient_csv, header =2, parse_dates = [0])
+    totals = check_totals(voc_df)
+    outliers = check_outliers(voc_df, summary_stats_df)
+    ratios = check_ratios(voc_df, mdls)
+    return ratios
+    
+#%% Main
+if __name__ == "__main__":
+    compound_by_type = sort_by_type(compound_categories = compound_categories)
+    test = monthly_screening_check(ambient_csv = r"D:\BV\working\validation\preprocessed_data\amount_crosstab_run_[S].csv",
+                                    summary_stats_cvs = r"D:\BV\working\validation\bv_summary_stats_2024.csv",
+                                    mdls = bv_mdls)
 
 
     
